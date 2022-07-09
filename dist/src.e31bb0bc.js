4682622b06c3ad5fef048ba12aa86ad05db1ff62
[Oluwatobi,Ojetoyinbo] <[Ojetoyinbotobi@gmail.com]>
2022-07-09 11:33:23 +0100

    testing deploy.....

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -36058,7 +36058,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62572" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52122" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
